71cf462aff1f151259cb0a0a9dad48d4bd2b18af
gullabi <bkulebi@gmail.com>
2018-10-10 13:53:24 +0200

    sequence aligner accepts lists, stdout convenient for two char elements

--- utils/seq_aligner.py
+++ utils/seq_aligner.py
@@ -20,7 +20,7 @@ gap_penalty      = -5 # both for opening and extanding
 def match_score(alpha, beta):
     if alpha == beta:
         return match_award
-    elif alpha == '-' or beta == '-':
+    elif alpha == '--' or beta == '--':
         return gap_penalty
     else:
         return mismatch_penalty
@@ -32,26 +32,26 @@ def finalize(align1, align2):
     i,j = 0,0
     
     #calcuate identity, score and aligned sequeces
-    symbol = ''
+    symbol = []
     found = 0
     score = 0
     identity = 0
     for i in range(0,len(align1)):
         # if two AAs are the same, then output the letter
-        if align1[i] == align2[i]:                
-            symbol = symbol + align1[i]
+        if align1[i] == align2[i]:
+            symbol.append(align1[i])
             identity = identity + 1
             score += match_score(align1[i], align2[i])
     
         # if they are not identical and none of them is gap
-        elif align1[i] != align2[i] and align1[i] != '-' and align2[i] != '-': 
+        elif align1[i] != align2[i] and align1[i] != '--' and align2[i] != '--':
             score += match_score(align1[i], align2[i])
-            symbol += ' '
+            symbol.append('  ')
             found = 0
     
         #if one of them is a gap, output a space
-        elif align1[i] == '-' or align2[i] == '-':          
-            symbol += ' '
+        elif align1[i] == '--' or align2[i] == '--':
+            symbol.append('  ')
             score += gap_penalty
     
     identity = float(identity) / len(align1) * 100
@@ -82,7 +82,7 @@ def needle(seq1, seq2):
             score[i][j] = max(match, delete, insert)
 
     # Traceback and compute the alignment 
-    align1, align2 = '', ''
+    align1, align2 = [], []
     i,j = m,n # start from the bottom right cell
     while i > 0 and j > 0: # end toching the top or the left edge
         score_current = score[i][j]
@@ -91,27 +91,27 @@ def needle(seq1, seq2):
         score_left = score[i-1][j]
 
         if score_current == score_diagonal + match_score(seq1[i-1], seq2[j-1]):
-            align1 += seq1[i-1]
-            align2 += seq2[j-1]
+            align1.append(seq1[i-1])
+            align2.append(seq2[j-1])
             i -= 1
             j -= 1
         elif score_current == score_left + gap_penalty:
-            align1 += seq1[i-1]
-            align2 += '-'
+            align1.append(seq1[i-1])
+            align2.append('--')
             i -= 1
         elif score_current == score_up + gap_penalty:
-            align1 += '-'
-            align2 += seq2[j-1]
+            align1.append('--')
+            align2.append(seq2[j-1])
             j -= 1
 
     # Finish tracing up to the top left cell
     while i > 0:
-        align1 += seq1[i-1]
-        align2 += '-'
+        align1.append(seq1[i-1])
+        align2.append('--')
         i -= 1
     while j > 0:
-        align1 += '-'
-        align2 += seq2[j-1]
+        align1.append('--')
+        align2.append(seq2[j-1])
         j -= 1
 
     finalize(align1, align2)
@@ -144,24 +144,24 @@ def water(seq1, seq2):
                 max_j = j
                 max_score = score[i][j];
     
-    align1, align2 = '', ''    # initial sequences
+    align1, align2 = [], []    # initial sequences
     
     i,j = max_i,max_j    # indices of path starting point
     
     #traceback, follow pointers
     while pointer[i][j] != 0:
         if pointer[i][j] == 3:
-            align1 += seq1[i-1]
-            align2 += seq2[j-1]
+            align1.append(seq1[i-1])
+            align2.append(seq2[j-1])
             i -= 1
             j -= 1
         elif pointer[i][j] == 2:
-            align1 += '-'
-            align2 += seq2[j-1]
+            align1.append('--')
+            align2.append(seq2[j-1])
             j -= 1
         elif pointer[i][j] == 1:
-            align1 += seq1[i-1]
-            align2 += '-'
+            align1.append(seq1[i-1])
+            align2.append('--')
             i -= 1
 
     finalize(align1, align2)
